3497d1cbf9010c2b48e9fea071bad90ed35b6af6
Caleb Alexander <cralexa@bgsu.edu>
2019-12-10 19:32:06 +0000

    first start

---
+++ readme.md
@@ -0,0 +1,7 @@
+TODO:
+    Get node.js to load
+    make a mysql database
+    learn:
+        node.js
+        npm
+        mysql
---
+++ src/scripts/GetSQL.js
@@ -0,0 +1,13 @@
+console.log("Hello js");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
